56511bae9a770b44debce6f3e99be7f6158fac2d
hassan <hassan.elouardy06@gmail.com>
2023-06-02 10:17:12 +0100

    Add component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Work />
     </div>
   );
 }
