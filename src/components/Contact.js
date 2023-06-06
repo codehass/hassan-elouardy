46af15b1663c535ec5107026fe88c1fb7395643f
hassan <hassan.elouardy06@gmail.com>
2023-06-06 15:01:49 +0100

    Edit the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './contact.css';
+import { useForm, ValidationError } from '@formspree/react';
 
-const Contact = () => (
-  <div className="contact">
-    <div className="contact-text">
-      <span>Have an ideo?</span>
-      I can help you to Start your project.
+const Contact = () => {
+  const [state, handleSubmit] = useForm('mnqykbbo');
+
+  return (
+    <div className="contact" id="contact">
+      <div className="contact-text">
+        <span>Have an ideo?</span>
+        I can help you to Start your project.
+      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" id="name" name="name" placeholder="Full Name" required="required" />
+        <input id="email" type="email" name="Email" placeholder="Email" required="required" />
+        <textarea id="textarea" name="message" placeholder="Message" required="required" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button type="submit" disabled={state.submitting}>Submit</button>
+      </form>
     </div>
-    <form className="form">
-      <input type="text" name="Full Name" placeholder="Full Name" />
-      <input type="text" name="Email" placeholder="Email" />
-      <textarea id="textarea" name="Message" placeholder="Message" />
-      <button type="button">Submit</button>
-    </form>
-  </div>
-);
+  );
+};
 
 export default Contact;
