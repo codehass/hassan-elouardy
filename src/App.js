8297ecc5a69f3e99c06985f2be08d7061af18a58
hassan <hassan.elouardy06@gmail.com>
2023-06-06 15:00:48 +0100

    Add about section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Work from './components/Work';
 import Feedback from './components/Feedback';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <div className="container">
         <Home />
+        <About />
         <Work />
         <Feedback />
         <Contact />
